75d71278f07c3d2096a9c16606cd54ce39d8eee6
HashimSaqib <hashim1saqib@gmail.com>
2024-08-15 15:00:08 +0500

    Add /gl/transaction_line route to list all GL Transactions Lines (Including AP & AR). TODO: Format Response & Update documentation

--- index.pl
+++ index.pl
@@ -95,6 +95,41 @@ my $api = $r->under('/api/client');
 ####                 ####
 #########################
 
+
+$api->get('/:client/gl/transaction_lines' => sub {
+    my $c      = shift;
+    my $params = $c->req->params->to_hash;
+    my $client = $c->param('client');
+
+    $c->slconfig->{dbconnect} = "dbi:Pg:dbname=$client";
+
+    my $form = new Form;
+    for ( keys %$params ) { $form->{$_} = $params->{$_} if $params->{$_} }
+    $form->{category} = 'X';
+
+    GL->transactions( $c->slconfig, $form );
+
+    # Check if the result is undefined, empty, or has no entries
+    if (  !defined $form->{GL}
+        || ref $form->{GL} ne 'ARRAY'
+        || scalar( @{ $form->{GL} } ) == 0 )
+    {
+        return $c->render(
+            status => 404,
+            json   => { error => "No transactions found" }
+        );
+    }
+
+    # Assuming $form->{GL} is an array reference with hash references
+    foreach my $transaction ( @{ $form->{GL} } ) {
+        delete $transaction->{$_}
+          for
+          qw(address address1 address2 city country entry_id name name_id zipcode);
+    }
+
+    $c->render( status => 200, json => $form->{GL} );
+});
+
 $api->get(
     '/:client/gl/transactions' => sub {
         my $c      = shift;
