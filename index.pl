5f51c38a4ff96e3c8e60d321cd6f5f419ccc950b
HashimSaqib <hashim1saqib@gmail.com>
2024-09-04 13:19:30 +0500

    Remove TS from general ledger

--- index.pl
+++ index.pl
@@ -11,6 +11,7 @@ use Mojo::Util qw(unquote);
 use Mojo::JSON qw(decode_json);
 use DBI;
 use DBIx::Simple;
+use SQL::Abstract; 
 use XML::Simple;
 use SL::Form;
 use SL::AM;
@@ -131,7 +132,7 @@ app->hook(around_dispatch => sub {
 #########################
 
 my $allMenuItems = qq'AR--AR;AR--Add Transaction;AR--Sales Invoice;AR--Credit Note;AR--Credit Invoice;AR--Reports;POS--POS;POS--Sale;POS--Open;POS--Receipts;Customers--Customers;Customers--Add Customer;Customers--Reports;AP--AP;AP--Add Transaction;AP--Vendor Invoice;AP--Debit Note;AP--Debit Invoice;AP--Reports;Vendors--Vendors;Vendors--Add Vendor;Vendors--Reports;Cash--Cash;Cash--Receipt;Cash--Receipts;Cash--Payment;Cash--Payments;Cash--Void Check;Cash--Reissue Check;Cash--Void Receipt;Cash--Reissue Receipt;Cash--FX Adjustment;Cash--Reconciliation;Cash--Reports;Vouchers--Vouchers;Vouchers--Payable;Vouchers--Payment;Vouchers--Payments;Vouchers--Payment Reversal;Vouchers--General Ledger;Vouchers--Reports;HR--HR;HR--Employees;HR--Payroll;Order Entry--Order Entry;Order Entry--Sales Order;Order Entry--Purchase Order;Order Entry--Reports;Order Entry--Generate;Order Entry--Consolidate;Logistics--Logistics;Logistics--Merchandise;Logistics--Reports;Quotations--Quotations;Quotations--Quotation;Quotations--RFQ;Quotations--Reports;General Ledger--General Ledger;General Ledger--Add Transaction;General Ledger--Reports;Goods & Services--Goods & Services;Goods & Services--Add Part;Goods & Services--Add Service;Goods & Services--Add Kit;Goods & Services--Add Assembly;Goods & Services--Add Labor/Overhead;Goods & Services--Add Group;Goods & Services--Add Pricegroup;Goods & Services--Stock Assembly;Goods & Services--Stock Adjustment;Goods & Services--Reports;Goods & Services--Changeup;Goods & Services--Translations;Projects & Jobs--Projects & Jobs;Projects & Jobs--Projects;Projects & Jobs--Jobs;Projects & Jobs--Translations;Reference Documents--Reference Documents;Reference Documents--Add Document;Reference Documents--List Documents;Image Files--Image Files;Image Files--Add File;Image Files--List Files;Reports--Reports;Reports--Chart of Accounts;Reports--Trial Balance;Reports--Income Statement;Reports--Balance Sheet;Recurring Transactions--Recurring Transactions;Batch--Batch;Batch--Print;Batch--Email;Batch--Queue;Exchange Rates--Exchange Rates;Import--Import;Import--Customers;Import--Vendors;Import--Parts;Import--Services;Import--Labor/Overhead;Import--Sales Invoices;Import--Groups;Import--Payments;Import--Sales Orders;Import--Purchase Orders;Import--Chart of Accounts;Import--General Ledger;Export--Export;Export--Payments;System--System;System--Defaults;System--Audit Control;System--Audit Log;System--Bank Accounts;System--Taxes;System--Currencies;System--Payment Methods;System--Workstations;System--Roles;System--Warehouses;System--Departments;System--Type of Business;System--Language;System--Mimetypes;System--SIC;System--Yearend;System--Maintenance;System--Backup;System--Chart of Accounts;System--html Templates;System--XML Templates;System--LaTeX Templates;System--Text Templates;Stylesheet--Stylesheet;Preferences--Preferences;New Window--New Window;Version--Version;Logout--Logout';
-my $neoLedgerMenu = qq'General Ledger--Reports;';
+my $neoLedgerMenu = qq'General Ledger--General Ledger;General Ledger--Add Transaction;General Ledger--Reports;';
 
 helper check_perms => sub {
     my ($c, $sessionkey, $permission) = @_;
@@ -747,8 +748,6 @@ sub api_gl_transaction () {
 
     warn $c->dumper($form);
 
-    my $ts =
-      $dbs->query( "SELECT ts from gl WHERE id = ?", $form->{id} )->hash->{ts};
 
     # Convert the Form object back into a JSON-like structure
     my $response_json = {
@@ -761,7 +760,6 @@ sub api_gl_transaction () {
         exchangeRate => $form->{exchangerate},
         transdate    => $form->{transdate},
         employeeId   => $form->{employee_id},
-        ts           => $ts,
         lines        => []
     };
 
@@ -897,7 +895,7 @@ $api->get('/:client/charts' => sub {
 
     # Execute the query with the necessary parameters
     my $entries = $dbs->query($sql, map { "%$_%" } @links)->hashes;
-    
+
     # Add the "label" property to each entry
     foreach my $entry (@$entries) {
         $entry->{label} = $entry->{accno} . '---' . $entry->{description};
